019a50b0fc1bd0c4c9c0a2cb8111b05ffe040d4b
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-09-01 18:27:40 -0400

    Introduce cumulative_energy_map()

--- PS1/matlab/SeamCarvingReduceWidth.m
+++ PS1/matlab/SeamCarvingReduceWidth.m
@@ -3,15 +3,23 @@ close all, clear all, clc;
 
 % Load the image and show the original work
 frame = imread('poe-raven.jpg');
-
+[h, w, ~] = size(frame);
 figure(1);
 imshow(frame)
 
 % Compute and acquire the energy map
-energyMap = energy_image(frameGray);
+% TODO - imgradient requires me to pass grayscale image!
+energyMap = energy_image(frame);
 figure(2);
 imshow(energyMap);
 
 % Calculate cumulative energy map
+cumulativeEnergyMap = cumulative_energy_map(energyMap, 'V');
+figure(3);
+imagesc(cumulativeEnergyMap);
+
+minCumulativeEnergy = min(cumulativeEnergyMap(h,:));
+
+
 
 
