40bce561092e34b4cba87287dbeae71057828af2
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-09-01 18:28:54 -0400

    Impelement dynamic programming to find the lowest cost seam

--- PS1/matlab/cumulative_energy_map.m
+++ PS1/matlab/cumulative_energy_map.m
@@ -1,3 +1,25 @@
+% TODO Add comments for the code and the function
 function [cumulativeEnergyMap] = cumulative_enegy_map(energyMap, direction)
+    if(nargin~=2)
+        errorMessage = 'Missing argument';
+        error(errorMessage);
+    end
+    
     cumulativeEnergyMap = zeros(size(energyMap));
+	minVal = 0;
+    
+    if('v' == lower(direction(1)))
+        for row=2:size(energyMap,1)
+            for col=1:size(energyMap,2)
+                if(col~=1)
+                    minVal= min(cumulativeEnergyMap(row-1,col-1), minVal);
+                elseif(col~=size(energyMap,2))
+                    minVal = min(cumulativeEnergyMap(row-1, col+1), minVal);
+                end
+                cumulativeEnergyMap(row,col) = energyMap(row,col)+min(cumulativeEnergyMap(row-1,col),minVal);
+            end
+        end
+    elseif('h' == lower(direction(1)))
+        % TODO Implement horizontal version
+    end
 end
