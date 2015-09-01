2c3bee2e1633b6ee9db1abaedfea691c40ff2fda
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-09-01 17:48:12 -0400

    Fix typo on file names

--- PS1/matlab/cumulative_enegy_map.m
+++
@@ -1,6 +0,0 @@
-function [output_args] = cumulative_enegy_map(input_args)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
---
+++ PS1/matlab/cumulative_energy_map.m
@@ -0,0 +1,3 @@
+function [cumulativeEnergyMap] = cumulative_enegy_map(energyMap, direction)
+    cumulativeEnergyMap = zeros(size(energyMap));
+end
