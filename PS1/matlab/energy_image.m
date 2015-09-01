a24b418ace2462ad419fc452f9aa5e81a2fe641b
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-09-01 18:29:43 -0400

    Convert frame to grayscale

--- PS1/matlab/energy_image.m
+++ PS1/matlab/energy_image.m
@@ -1,5 +1,6 @@
+% TODO Add comments for the code and the function
 function energyMap = enegy_image(im)
-    frameGray = rgb2gray(frame);
-    [energyMap, ~] = imgradient(im, 'prewitt'); 
+    frameGray = rgb2gray(im);
+    [energyMap, ~] = imgradient(frameGray, 'prewitt'); 
 end
 
