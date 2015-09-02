9707b4c217e6e3daa494a00ce9063d63fea94bc1
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-09-02 11:19:15 -0400

    Fix bug in the search algorithm

--- PS1/matlab/find_optimal_horizontal_seam.m
+++ PS1/matlab/find_optimal_horizontal_seam.m
@@ -1,14 +1,10 @@
 % TODO Add comments for the code and the function
 function horizontalSeam = find_optimal_horizontal_seam(cumulativeEnergyMap)
     [h, w, ~] = size(cumulativeEnergyMap);    
-    horizontalSeam = zeros(2, w);
-    horizontalSeam(1,w) = w;
-    [~, horizontalSeam(2,w)] = min(cumulativeEnergyMap(:,w));
-    for col=w-1:1
-        index = horizontalSeam(2, w)-1:horizontalSeam(2, w)+1;
-        index(index<1) = [];
-        index(index>w) = [];
-        horizontalSeam(2, col) = cumulativeEnergyMap(index,col);
-        horizontalSeam(1, col) = col;
+    horizontalSeam = zeros(h,1);
+    [~, horizontalSeam(h)] = min(cumulativeEnergyMap(:,w));
+    for col=w-1:-1:1
+        index = max(horizontalSeam(col+1)-1,1):min(horizontalSeam(col+1)+1,w);
+        [~, horizontalSeam(col)] = min(cumulativeEnergyMap(index, col));
     end    
 end
