d76b043767a1830d5719cc9bdc6d7bd5fb293e2c
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-09-03 10:57:07 -0400

    Seperate resizing operations into two files, ...reduceHeight.m and ...reduceWidth.m

---
+++ PS1/matlab/SeamCarvingReduceHeight.m
@@ -0,0 +1,22 @@
+% Clear command window and workspace, close all figures
+close all, clear all, clc;
+
+% Load the image and show the original work
+frame = imread('inputSeamCarvingPrague.jpg');
+% frame = imread('fsm.jpg');
+[h, w, c] = size(frame);
+
+% Compute and acquire the energy map
+% TODO - imgradient requires me to pass grayscale image!
+energyMap = energy_image(frame);
+figure(1);
+imshow(frame);
+newImage = frame;
+newEnergyMap = energyMap;
+
+for k=1:100
+    [newImage, newEnergyMap] = reduceHeight(newImage,newEnergyMap);
+end
+
+figure(99);
+imshow(newImage);
