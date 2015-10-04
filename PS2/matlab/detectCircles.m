5ea518099d860a6ba84f8ebd366fa0be3693596a
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-10-03 20:26:24 -0400

    Compelete generic hough transformation

--- PS2/matlab/detectCircles.m
+++ PS2/matlab/detectCircles.m
@@ -2,23 +2,34 @@ function [centers, ind] = detectCircles(im, radius, useGradient)
     im = rgb2gray(im);
     [h, w, ~] = size(im);
     [Gmag,Gdir] = imgradient(im);
-    edges = Gmag>mean(Gmag(:));
-    
+    edges = edge(im, 'Canny');
+
+    Gdir = Gdir * 0.0175 + pi;
+
     [i, j] = find(edges == 1);
-    
+
     thetaResolution = 0.1;
     angle = 0:thetaResolution:2*pi;
-    angleIndex = 1:numel(theta);
-    houghSpace = zeros(h,w,numel(theta));
-    
+    angleIndex = 1:numel(angle);
+    houghSpace = zeros(h,w);
+
     for counter=1:numel(i)
-        center.x = i(counter); center.y = j(counter);
+        center.y = i(counter);
+        center.x = j(counter);
         if(useGradient == 1)
-            theta = 
+            theta = Gdir(center.y, center.x);
+        else
+            theta = angle;
         end
-            
-        a = center.x -radius * cos(theta)
-                
+
+        for rad=1:numel(radius)
+            a = center.x + radius(rad) * cos(theta);
+            b = center.y + radius(rad) * sin(theta);
+            a(a>w | a<1) = [];
+            b(b>h | b<1) = [];
+            houghSpace(round(b), round(a)) = houghSpace(round(b), round(a)) + 1;
+        end    
     end
-    
+    figure; imshow(edges);
+    figure; imagesc(houghSpace);
 end
--- PS2/matlab/hough.m
+++ PS2/matlab/hough.m
@@ -1,49 +1,13 @@
-clc; clear all; close all; profile on;
+ clc; clear all; close all; profile on;
 
 im = imread('jupiter.jpg');
 [h, w, ~] = size(im);
-
+maxDim = max(h,w);
 frame = zeros(h,w, 'uint8');
 
-radius = 150;
-% center.x = 200;
-% center.y = 200;
-useGradient = 1;
-% detectCircles(im,10,1);
+radius = 50;
+useGradient = 0;
 
-im = rgb2gray(im);
-[h, w, ~] = size(im);
-[Gmag,Gdir] = imgradient(im);
-edges = Gmag>mean(Gmag(:));
 
-Gdir = Gdir * 0.0175 + pi;
+detectCircles(im, maxDim, 0);
 
-[i, j] = find(edges == 1);
-
-thetaResolution = 0.1;
-angle = 0:thetaResolution:2*pi;
-angleIndex = 1:numel(angle);
-houghSpace = zeros(h,w);
-
-for counter=1:numel(i)
-    center.y = i(counter);
-    center.x = j(counter);
-    if(useGradient == 1)
-        theta = Gdir(center.y, center.x);
-    else
-        theta = angle;
-    end
-           
-    a = center.x + radius * cos(theta);
-    b = center.y + radius * sin(theta);
-    a(a>w | a<1) = [];
-    b(b>h | b<1) = [];
-    
-    houghSpace(round(b), round(a)) = houghSpace(round(b), round(a)) + 1;
-    
-%     for votingCounter=1:numel(theta)
-%         [~, idx] = min(abs(angle-theta(votingCounter)));
-%         ['a: ', num2str(round(a)), ' b: ', num2str(round(b)), ' theta: ', num2str(theta), ' idx: ', num2str(idx)];
-%         houghSpace(round(b), round(a));
-%     end
-end
