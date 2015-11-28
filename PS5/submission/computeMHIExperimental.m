6eca547b855b79ba540a94bd64c23a087bb7ab41
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2015-11-27 22:10:09 -0500

    Final Commit

---
+++ PS5/submission/computeMHIExperimental.m
@@ -0,0 +1,40 @@
+function H = computeMHIExperimental(address)
+%   Get the depth file locations
+    depthfiles = dir([address, '/*.pgm']);
+    nFrames = length(depthfiles);
+%   Thresholds moving pixels and the lenght of the sequence will be used.
+    tau = nFrames;
+    threshold = 50000;    
+    previousFrame = imread([address, depthfiles(1).name]);
+    previousFrame = previousFrame > threshold;
+    D = zeros(nFrames, 480, 640, 'uint8');
+    H = zeros(nFrames, 480, 640, 'uint8');
+
+    for i=2:nFrames        
+        depth = imread([address, depthfiles(i).name]);
+        depth = depth > threshold;
+        assignin('base','depth', depth);
+        diff = min(previousFrame, depth);
+        indices = ~diff;
+        assignin('base','indices', indices);
+%   Binary image sequence indicating motion locations (moving pixels)
+        D(i, indices) = 255;
+        H(i, indices) = tau;
+        H(i, ~indices) = H(i-1, ~indices)-1;
+        previousFrame = depth;
+%%  Visualization for debug purposes
+%         figure(1); imagesc(reshape(D(i,:),480,640));
+%         figure(2); imagesc(reshape(H(i,:),480,640));
+%%  Morphological Filters
+%         se = strel('square', 3);
+%         afterOpening = imerode(diff,se);
+%         figure(2); imshow(afterOpening);
+    end
+%   Extend energy image and motion history image to the workspace (DEBUG)
+%     assignin('base','D', D);
+    assignin('base','H_series', H);
+    H = double(H);
+    H = H(nFrames,:)/max(H(nFrames,:));
+    H =reshape(H, 480,640);
+end
+
---
+++ PS5/submission/predictAction.m
@@ -0,0 +1,20 @@
+function predictedLabel = predictAction(testMoments, trainMoments, trainLabels)
+    predictedLabel = -1;
+    [h,~] = size(trainMoments);
+    distances = zeros(h,1);
+%     sequenceMatched = 1:h;
+    % Calculate variances for normalized euclidian distance
+    variance = nanvar(trainMoments);
+    for i=1:h
+        % calculate distances
+        distances(i) = sqrt(sum(((trainMoments(i,:)-testMoments).^2)./variance));
+    end
+    [Y, I] = sort(distances);
+%     Confusion Matrix for Murat.
+%     predictedLabel = trainLabels(I(2));
+    predictedLabel = mode(trainLabels(I(1:4)));
+    assignin('base','label', I);
+    assignin('base','distances', Y);
+%     assignin('base','sequenceMatched', sequenceMatched(I));
+end
+
